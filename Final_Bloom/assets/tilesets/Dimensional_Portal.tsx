781ab8ec70343e058175aba6a8be2bfd15c1460a
TRUMPETMONK]E\745 <caleb.jacobs@kingsway.school.nz>
2025-09-08 12:53:39 +1200

    fixed map
    adjusted position and some weird areas

---
+++ Final_Bloom/assets/tilesets/Dimensional_Portal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Dimensional_Portal" tilewidth="16" tileheight="16" tilecount="24" columns="6">
+ <image source="../Tileset images/Dimensional_Portal.png" width="96" height="64"/>
+</tileset>
